2872e926ac9ee7ffef8a6ebf69b13e3e57e33c61
Ton Poppe <info@festina-lente.biz>
2023-07-14 14:09:15 +0200

    refac grahpsage

--- graphsage/train_messaging_adj.py
+++ graphsage/train_messaging_adj.py
@@ -25,68 +25,73 @@ torch.cuda.seed_all()
 
 # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"]="4"
+
+def get_id(node, node_to_id):
+        """check if node is in node_to_id dict and adds when missing"""
+        node = int(node)
+        if node not in node_to_id:
+            node_id = len(node_to_id)
+            node_to_id[node] = node_id
+            return node_id
+        else:
+            return node_to_id[node]
+
+def train_and_calculate_graphsage_embedding(data, features, output_path):
+    """trains graphsage and stores trained model with embedding in the output path"""
+    node_to_id = dict()  # dictionary with node ids
+    
+    # Built dict containing a set of neighsbors per node id. 
+    neighbors_dict = defaultdict(set) 
+    for start, end in data[['start','end']].values:
+        start_id = get_id(start, node_to_id)
+        end_id = get_id(end, node_to_id)
+        neighbors_dict[start_id].add(end_id)
+        neighbors_dict[end_id].add(start_id)
+
+    # prep feature matrix
+    node_mapping_df = pd.DataFrame.from_dict(node_to_id, orient='index', columns=['new_id'])
+    node_mapping_df = node_mapping_df.reset_index(names='id')
+    features = features.merge(node_mapping_df, on='id', how='inner')
+    features = features.drop(['id', 'new_id'], axis=1)
+
+    _N = features.shape[0]
+    _M = data.shape[0]
+    assert _N==len(node_to_id), "N is different from the number of node id's"
+
+
+    # calculate embedding
+    embedding_dim=128
+    epoch = 200  #20000  # epoch for training
+    boost_epoch=400 #4000 # epoch per boosting run
+    boost_times = 3 #100 # number of boosting runs
+    import time
+    # import utils_graphsage_1
+    import importlib
+    importlib.reload(utils_graphsage_1)
+
+    graphsagemodel=utils_graphsage_1.GraphSAGE(_N=_N,
+                                feat_matrix = features.values,
+                                adj_dic=neighbors_dict,
+                                embedding_dim=embedding_dim,
+                                verbose_level=2)
+
+    start = time.process_time_ns()
+    graphsagemodel.graphsage_train(boost_times=boost_times,  # number of boosting runs.
+                                add_edges=1000,  # number of edges added during boosting
+                                training_epoch=epoch,  # epoch for normal training
+                                boost_epoch=boost_epoch,  # epoch for boosting with added edges.
+                                learning_rate=0.001,
+                                save_number=0,
+                                dirs=output_path)
+    
+    end = time.process_time_ns()
+    print(f"duration {(end-start)/1e9} sec")
+    #graphsagemodel.save_model(path='graphsage_model_node_type/graph_graphsage.pth',embedding_path='graphsage_model_node_type/embeddings.npy')
+
+# %%
+output_path = 'models/ucsocial'
 data= pd.read_csv("../data/opsahl-ucsocial/data.csv")
 data = data.drop_duplicates(subset=['start','end'])
 features = pd.read_parquet("../data/opsahl-ucsocial/node_features.parquet")
-
-
-#%% create mapping dict from node to id
-node_to_id = dict()  # dictionary with node ids
-def get_id(node, node_to_id):
-    """check if node is in node_to_id dict and adds when missing"""
-    node = int(node)
-    if node not in node_to_id:
-        node_id = len(node_to_id)
-        node_to_id[node] = node_id
-        return node_id
-    else:
-        return node_to_id[node]
-
-#%% Built dict containing a set of neighsbors per node id. 
-neighbors_dict = defaultdict(set) 
-for start, end in data[['start','end']].values:
-    start_id = get_id(start, node_to_id)
-    end_id = get_id(end, node_to_id)
-    neighbors_dict[start_id].add(end_id)
-    neighbors_dict[end_id].add(start_id)
-
-
-#%% prep feature matrix
-node_mapping_df = pd.DataFrame.from_dict(node_to_id, orient='index', columns=['new_id'])
-node_mapping_df = node_mapping_df.reset_index(names='id')
-features = features.merge(node_mapping_df, on='id', how='inner')
-features = features.drop(['id', 'new_id'], axis=1)
-
-_N = features.shape[0]
-_M = data.shape[0]
-assert _N==len(node_to_id), "N is different from the number of node id's"
-
-
-#%%
-embedding_dim=128
-epoch = 200  #20000  # epoch for training
-boost_epoch=400 #4000 # epoch per boosting run
-boost_times = 3 #100 # number of boosting runs
-import time
-# import utils_graphsage_1
-import importlib
-importlib.reload(utils_graphsage_1)
-
-graphsagemodel=utils_graphsage_1.GraphSAGE(_N=_N,
-                               feat_matrix = features.values,
-                               adj_dic=neighbors_dict,
-                               embedding_dim=embedding_dim,
-                               verbose_level=2)
-
-start = time.process_time_ns()
-graphsagemodel.graphsage_train(boost_times=boost_times,  # number of boosting runs.
-                               add_edges=1000,  # number of edges added during boosting
-                               training_epoch=epoch,  # epoch for normal training
-                               boost_epoch=boost_epoch,  # epoch for boosting with added edges.
-                               learning_rate=0.001,
-                               save_number=0,dirs='models/ucsocial')
-end = time.process_time_ns()
-print(f"duration {(end-start)/1e9} sec")
-#graphsagemodel.save_model(path='graphsage_model_node_type/graph_graphsage.pth',embedding_path='graphsage_model_node_type/embeddings.npy')
-
+train_and_calculate_graphsage_embedding(data, features, output_path)
 # %%
